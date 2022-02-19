3431b1efc21b06013a17548e04e6e67fc1417847
wsl <w15173187105@163.com>
2022-02-19 22:41:52 +0800

    fix bug

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'mobx-react'
+import store from './store'
 
 import PlayerIndex from './pages/player/Index'
 import FilmsIndex from './pages/films/Index'
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <PlayerIndex />
     </Provider>
   );
