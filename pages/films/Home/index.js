c6611f02f239a4793b9a6f29bdfa24862d7b5732
wangshuli <w15173187105@163.com>
2022-02-24 13:21:36 +0800

    feat serachHeader

--- pages/films/Home/index.js
+++ pages/films/Home/index.js
@@ -1,12 +1,13 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
+import SafeAreaView from 'react-native-safe-area-view'; 
 
 export default class home extends Component {
   render() {
     return (
-      <View>
+      <SafeAreaView>
         <Text>home</Text>
-      </View>
+      </SafeAreaView>
     )
   }
 }
