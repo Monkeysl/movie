35e36239b0c0dad45c3fa75e4b82a67c04254a57
wsl <w15173187105@163.com>
2022-02-25 21:30:23 +0800

    feat movie

--- pages/films/Home/index.js
+++ pages/films/Home/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
 import SafeAreaView from 'react-native-safe-area-view'; 
 import { Tabs } from '@ant-design/react-native'
@@ -72,6 +72,31 @@ export default class Home extends Component {
                 <Image style={styles.noticeIcon} source={require('../../../assets/images/notice.png')} />
                 <Text style={styles.noticeText}>最新通知咨询都在这里了</Text>
               </View>
+              <View style={styles.newHotContainer}>
+                <View style={styles.newHotTitle}>
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Image style={{ width:22, height: 22 }} source={require('../../../assets/images/hot.png')} />
+                    <Text style={{ fontSize: 15 }}>最新热门</Text>
+                    <Image style={{ width: 12, height: 15 }} source={require('../../../assets/images/flame.png')} />
+                  </View>
+                  <View><Text style={{ color: '#777878', fontSize: 12 }}>共200部&gt;</Text></View>
+                </View>
+
+                <View style={styles.newHotListCon}>
+                  <View styles={styles.newHotItem}>
+                    <View style={styles.movieCon}>
+                      <ImageBackground style={{ width: 110, height: 150 }} source={require('../../../assets/images/earth.jpg')}>
+                        <Text>更新至12集</Text>
+                      </ImageBackground>
+                    </View>
+                    <Text>电影名称</Text>
+                  </View>
+                </View>
+              </View>
+              <View>
+
+              </View>
+
             </View>
             <View style={styles.tabCon}>
               <Text>Content of Second Tab</Text>
@@ -108,13 +133,53 @@ const styles = StyleSheet.create({
   },
 
   notice: {
-    width: 12,
-    height: 15,
+    flexDirection: 'row',
+    paddingHorizontal: 19,
+    paddingTop: 7,
+    paddingBottom: 7,
+    alignItems: 'center',
+    backgroundColor: '#eee'
+    // justifyContent: 'space-between'
   },
   noticeIcon: {
+    width: 12,
+    height: 15,
 
   },
   noticeText: {
-    
+    color: '#0e050a',
+    fontSize: 12,
+    marginLeft: 6
+  },
+  newHotContainer: {
+
+  },
+  newHotTitle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingTop: 20,
+  },
+  newHotListCon: {
+    paddingHorizontal: 7,
+  },
+  newHotItem: {
+    marginHorizontal: 8,
+  },
+  movieCon: {
+    position: 'relative',
+    borderRadius: 15,
+    overflow: 'hidden',
+    width: 110,
+    height: 150,
+  },
+  moiveInfo: {
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: 21,
+    backgroundColor: 'rgba(0,0,0,.3)'
   }
 });
